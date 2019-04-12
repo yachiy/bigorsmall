05c6aeff3b12fd71eceacb636e27bbb477607d80
yachida <k-yachida@atware.co.jp>
2019-04-12 16:30:35 +0900

    賭けのモデルに予想を追加

--- src/main/java/yachiy/bigorsmall/domain/Bet.java
+++ src/main/java/yachiy/bigorsmall/domain/Bet.java
@@ -1,29 +1,35 @@
 package yachiy.bigorsmall.domain;
 
 /**
- * 賭け金を表すモデル
+ * 賭けを表すモデル
  */
 public class Bet {
     private static final int MIN_BET = 1;
+    private static final int LOSE_REFUND = 0;
     private static final double WINNING_RATE = 1.5;
 
+    private Predict predict;
     private int value;
 
-    public Bet(int value) {
+    public Bet(Predict predict, int value) {
         if (value < MIN_BET) {
             throw new IllegalArgumentException(
               String.format("最低%s以上の賭け金を設定してください（値:%s）", MIN_BET, value));
         }
+        this.predict = predict;
         this.value = value;
     }
 
     /**
-     * 賭けに当選した際の払い戻し額を返却します。
+     * 払い戻し額を返却します。
      * 小数点以下は切捨てます。
      *
      * @return 払い戻し額
      */
-    public int winningAmount() {
+    public int refund(Predict answer) {
+        if (predict.equals(answer)) {
+            return LOSE_REFUND;
+        }
         return (int) (value * WINNING_RATE);
     }
 }
